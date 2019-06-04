800ae30d248fc975b8c80c15a2f5eba353d95f61
wuhuanhost <post.thinking@gmail.com>
2019-06-04 11:40:12 +0800

    '调整了项目接口试用了路由跳转'

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,26 @@ import React from "react";
 import "./style/Home.scss";
 import ADD from "./containers/ADD";
 import ToDo from "./containers/TodoList";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+const { SubMenu } = Menu;
 const { Header, Sider, Content } = Layout;
-
 class SiderDemo extends React.Component {
+    rootSubmenuKeys = ["sub1", "sub2", "sub4"];
+
     state = {
+        openKeys: ["sub1"],
         collapsed: false
+    };
+
+    onOpenChange = openKeys => {
+        const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
+        if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            this.setState({ openKeys });
+        } else {
+            this.setState({
+                openKeys: latestOpenKey ? [latestOpenKey] : []
+            });
+        }
     };
 
     toggle = () => {
@@ -18,42 +33,86 @@ class SiderDemo extends React.Component {
 
     render() {
         return (
-            <Layout id="components-layout-demo-custom-trigger">
-                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-                        <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span>nav 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="video-camera" />
-                            <span>nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="upload" />
-                            <span>nav 3</span>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Header style={{ background: "#fff", padding: 0 }}>
-                        <Icon className="trigger" type={this.state.collapsed ? "menu-unfold" : "menu-fold"} onClick={this.toggle} />
-                    </Header>
-                    <Content
-                        style={{
-                            margin: "24px 16px",
-                            padding: 24,
-                            background: "#fff",
-                            minHeight: 280
-                        }}
-                    >
-                        Content
-                        <ADD />
-                        <ToDo />
-                    </Content>
+            <Router>
+                <Layout id="components-layout-demo-custom-trigger">
+                    <Sider trigger={null} collapsible collapsed={this.state.collapsed} width={256} collapsedWidth={80}>
+                        <div className="logo" />
+                        <Menu mode="inline" theme="dark" defaultOpenKeys={["sub1"]} defaultSelectedKeys={["1"]} onOpenChange={this.onOpenChange}>
+                            <SubMenu
+                                collapsed={false}
+                                key="sub1"
+                                title={
+                                    <span>
+                                        <Icon type="mail" />
+                                        <span>Navigation One</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="1">
+                                    <Link to="/test">文件系统</Link>
+                                </Menu.Item>
+                                <Menu.Item key="2">
+                                    <Link to="/test1">文件系统</Link>
+                                </Menu.Item>
+                                <Menu.Item key="3">Option 3</Menu.Item>
+                                <Menu.Item key="4">Option 4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub2"
+                                title={
+                                    <span>
+                                        <Icon type="appstore" />
+                                        <span>Navigation Two</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="5">Option 5</Menu.Item>
+                                <Menu.Item key="6">Option 6</Menu.Item>
+                                <SubMenu key="sub3" title="Submenu">
+                                    <Menu.Item key="7">Option 7</Menu.Item>
+                                    <Menu.Item key="8">Option 8</Menu.Item>
+                                </SubMenu>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub4"
+                                title={
+                                    <span>
+                                        <Icon type="setting" />
+                                        <span>Navigation Three</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="9">Option 9</Menu.Item>
+                                <Menu.Item key="10">Option 10</Menu.Item>
+                                <Menu.Item key="11">Option 11</Menu.Item>
+                                <Menu.Item key="12">Option 12</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+
+                    <Layout>
+                        <Header style={{ background: "#fff", padding: 0 }}>
+                            <Icon className="trigger" type={this.state.collapsed ? "menu-unfold" : "menu-fold"} onClick={this.toggle} />
+                        </Header>
+                        <Content
+                            style={{
+                                margin: "24px 16px",
+                                padding: 24,
+                                background: "#fff",
+                                minHeight: 280
+                            }}
+                        >
+                            Content
+                            {/* <ADD />
+                            <ToDo /> */}
+                            <Switch>
+                                <Route path="/test" component={ADD} />
+                                <Route path="/test1" component={ToDo} />
+                            </Switch>
+                        </Content>
+                    </Layout>
                 </Layout>
-            </Layout>
+            </Router>
         );
     }
 }
